71c455c5015df826024fa90c4f9947733ee84312
liorsachakov <liorsac@wix.com>
2025-05-24 19:56:37 +0300

    Update OpenAPI documentation and implement new recipe search and add endpoints. Refactor existing routes for clarity and consistency, including renaming paths and adding query parameters for recipe searches.

--- routes/recipes.js
+++ routes/recipes.js
@@ -4,7 +4,6 @@ const recipes_utils = require("./utils/recipes_utils");
 
 router.get("/", (req, res) => res.send("im here"));
 
-
 router.get("/random", async (req, res, next) => {
   console.log("enterd to random recipes first");
   try {
@@ -18,7 +17,7 @@ router.get("/random", async (req, res, next) => {
 /**
  * This path returns a full details of a recipe by its id
  */
-router.get("/:recipeId", async (req, res, next) => {
+router.get("/searchById/:recipeId", async (req, res, next) => {
   try {
     const recipe = await recipes_utils.getRecipeDetails(req.params.recipeId);
     res.send(recipe);
@@ -27,12 +26,25 @@ router.get("/:recipeId", async (req, res, next) => {
   }
 });
 
+router.get("/search", async (req, res, next) => { 
+  try {
+    const recipes = await recipes_utils.getRecipeByText(req.query.query, req.query.number || 5);
+    res.send(recipes);
+  } catch (error) {
+    next(error);
+  }
+});
 
-
-
-
-
-
-
+/**
+ * This path gets recipe data in the body and saves it to the database
+ */
+router.post("/addRecipe", async (req, res, next) => {
+  try {
+    await recipes_utils.addRecipe(req.body);
+    res.status(201).send("Recipe successfully added to database");
+  } catch (error) {
+    next(error);
+  }
+});
 
 module.exports = router;
