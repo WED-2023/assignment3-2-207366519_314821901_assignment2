53caf7c9e9d35bd8c828906c6317cb66b003d437
asafbaro <asafbaro@post.bgu.ac.il>
2025-06-14 21:03:17 +0300

    DB change

--- sql scripts/create.sql
+++ sql scripts/create.sql
@@ -7,8 +7,8 @@ CREATE TABLE IF NOT EXISTS users (
   lastname VARCHAR(50) NOT NULL,
   country VARCHAR(50) NOT NULL,
   password VARCHAR(255) NOT NULL,
-  email VARCHAR(100) NOT NULL UNIQUE,
-);
+  email VARCHAR(100) NOT NULL UNIQUE
+  );
 
 
 create table if not exists receipes (
